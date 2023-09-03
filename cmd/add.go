c719b8489a8b85726075fb949d9c38fae8c44ba3
hrit99 <hritikpihal51299@gmail.com>
2023-09-03 23:28:02 +0530

    edited readme.md file.

--- cmd/add.go
+++ cmd/add.go
@@ -1,17 +1,8 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+Copyright © 2023 Hritik Pihal <hritikpihal51299@gmail.com>
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-	http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+add command is used after family-tree to add a new person and store it. It is also used to add a new relation and store it.
+The format goes like family-tree add <sub-Command> <name>
 */
 package cmd
 
--- cmd/count.go
+++ cmd/count.go
@@ -1,17 +1,6 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-	http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+This command is used to count the number of relatives with the specified relation of a person.
+The format for the command is family-tree count <relation> of <person>
 */
 package cmd
 
@@ -25,7 +14,7 @@ import (
 var countCmd = &cobra.Command{
 	Use:   "count",
 	Short: "Used to count number of specific relatives",
-	Long: `THis command is used to count the number of relatives with the specified relation of a person.
+	Long: `This command is used to count the number of relatives with the specified relation of a person.
 	The format for the command is family-tree count <relation> of <person>`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if args[1] == "of" {
